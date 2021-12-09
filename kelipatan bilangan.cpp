1ca84e3120dbe5fc789c8020d5756b4e9f18533b
CELY FEBITA ILHAM <92569794+CelyFebitaIlham@users.noreply.github.com>
2021-12-09 08:48:59 +0700

    Add files via upload

---
+++ kelipatan bilangan.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+int main (){
+	
+	cout << "CELY FEBITA ILHAM" << endl;
+	cout << "2117051022"	    << endl;
+	cout << "D"                 << endl;
+	
+	cout << "Program Kelipatan 3 Dari Bilang i sampai n" <<endl;
+	
+	/* dik = i : bil_awal
+		     n : bil_akhir */
+	
+	int bil_awal;
+	int bil_akhir;
+	int a ; 
+	cout << "Bilangan Awal =";
+	cin >> bil_awal;
+	cout << "bilangan Akhir =";
+	cin >> bil_akhir;
+	cout<<endl;
+	
+
+for( a=bil_awal ; a<= bil_akhir ; a+=3){
+	cout << a << endl;
+}
+
+}
+
+
+	
+
